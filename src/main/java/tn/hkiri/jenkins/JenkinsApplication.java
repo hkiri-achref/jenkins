fef26d0e7219d345b5689a448153cc70a5e080a4
Hkiri Achref <achref.hkiri2@gmail.com>
2022-12-26 16:53:31 +0100

    added few logs and one test

--- src/main/java/tn/hkiri/jenkins/JenkinsApplication.java
+++ src/main/java/tn/hkiri/jenkins/JenkinsApplication.java
@@ -1,13 +1,24 @@
 package tn.hkiri.jenkins;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JenkinsApplication {
 
+
+
+	@PostConstruct
+	public void init(){
+		log.info("Application starting ...");
+	}
 	public static void main(String[] args) {
+		log.info("Application Executed ....");
 		SpringApplication.run(JenkinsApplication.class, args);
+
 	}
 
 }
--- src/test/java/tn/hkiri/jenkins/JenkinsApplicationTests.java
+++ src/test/java/tn/hkiri/jenkins/JenkinsApplicationTests.java
@@ -1,13 +1,19 @@
 package tn.hkiri.jenkins;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@Slf4j
 class JenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("executing unit testing");
+		assertEquals(true,true);
 	}
 
 }
